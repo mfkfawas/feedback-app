3016e98104aad0dbb528fc5ac0c6330f7130ab0d
mfkfawas <mfkfawas@gmail.com>
2022-03-02 20:52:54 +0530

    new feature: delete feedback

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = id => {
+    if (
+      window.confirm('Are you sure you want to delete this feedback?')
+    )
+      setFeedback(feedback.filter(item => item.id !== id));
+  };
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackList
+          feedback={feedback}
+          handleDelete={deleteFeedback}
+        />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) return <p>No Feedback</p>;
 
   return (
     <div className='feedback-list'>
       {feedback.map(item => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}
+        />
       ))}
     </div>
   );
