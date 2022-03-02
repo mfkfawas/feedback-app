1e753b411de63498d00936b5f9dec16622a7f0cf
mfkfawas <mfkfawas@gmail.com>
2022-03-02 17:33:44 +0530

    added proptypes to components

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,26 @@
-import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types';
+import FeedbackItem from './FeedbackItem';
 
 const FeedbackList = ({ feedback }) => {
-  if (!feedback || feedback.length === 0) return <p>No Feedback</p>
+  if (!feedback || feedback.length === 0) return <p>No Feedback</p>;
 
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
+      {feedback.map(item => (
         <FeedbackItem key={item.id} item={item} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default FeedbackList
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FeedbackList;
---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+const Card = ({ children, reverse }) => {
+  return (
+    <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+  );
+};
+
+Card.defaultProps = {
+  reverse: false,
+};
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+};
+
+export default Card;
